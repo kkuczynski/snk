74b4d81e6023a4f283d1ad062822ec4a7884f42e
kkuczynski <krzysiek.kuczynski@icloud.com>
2020-10-07 12:45:40 +0200

    Changed the way game starts, added restart

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -20,14 +20,15 @@ export class HomePage implements OnInit {
   private _canvasBorderWidth: number = 2;
   private _gameArray = [];
   private _numberOfSquaresVertically: number;
-  private _moveJ: number = -1;
+  private _moveJ: number = 1;
   private _moveI: number = 0;
   private _headI: number;
   private _headJ: number;
   private _headValue: number;
-  private _playing: boolean = true;
+  private _playing: boolean = false;
   private _canTurn: boolean;
   private _numberOfTreats: number = 0;
+  private _gameFinished: boolean = false;
 
   constructor(
     private _platform: Platform,
@@ -53,13 +54,9 @@ export class HomePage implements OnInit {
     this._platform.ready().then(() => {
       this._width = this._platform.width() - 10;
       this._height = this._platform.height() - this._top;
-      console.log(this._width);
-      console.log(this._height);
       if (this._width * this._heightToWidthRatio > this._height) {
         this._width = Math.floor(this._height / this._heightToWidthRatio);
         this._height = this._width * this._heightToWidthRatio;
-        console.log(this._width);
-        console.log(this._height);
       } else {
         this._height = Math.floor(this._width * this._heightToWidthRatio);
         this._width = this._height / this._heightToWidthRatio;
@@ -68,8 +65,6 @@ export class HomePage implements OnInit {
       this._width = this._squareSize * this._numberOfSquaresHorizontally;
       this._height = this._width * this._heightToWidthRatio;
       this._left = ((this._platform.width() - this._width) / 2) - this._canvasBorderWidth;
-      console.log(this._width);
-      console.log(this._height);
       this.setCanvas();
       this.drawCanvas();
     });
@@ -77,14 +72,11 @@ export class HomePage implements OnInit {
 
   setCanvas() {
     this._canvas = document.getElementById('canvas') as HTMLCanvasElement;
-    console.log(this._canvas);
-
     this._canvas.setAttribute('width', this._width.toString() + 'px');
     this._canvas.setAttribute('height', this._height.toString() + 'px');
     this._canvas.setAttribute('top', this._top.toString() + 'px');
     this._canvas.style.left = this._left.toString() + 'px';
     this._ctx = this._canvas.getContext("2d");
-    console.log(this._ctx);
   }
 
   handleDarkTheme() {
@@ -138,6 +130,7 @@ export class HomePage implements OnInit {
       for (let i = 0; i < this._numberOfSquaresVertically; i++) {
         for (let j = 0; j < this._numberOfSquaresHorizontally; j++) {
           this._gameArray[i][j] = 0;
+          this.initSnake();
         }
       }
     } else {
@@ -149,20 +142,21 @@ export class HomePage implements OnInit {
         this._gameArray.push(rowArray);
       }
     }
-    console.log(this._gameArray)
   }
 
   initSnake() {
-    let i = Math.floor(this._numberOfSquaresVertically / 2);
-    let x = 6;
+    this._numberOfTreats = 0;
+    let i = Math.floor(this._numberOfSquaresVertically / 2)-8;
+    let x = 1;
+    
     for (let j = Math.floor((this._numberOfSquaresHorizontally / 2) - 3); j < Math.floor((this._numberOfSquaresHorizontally / 2) + 3); j++) {
       if (x === 6) {
         this._headI = i;
-        this._headJ = j;
-        this._headValue = x;
+        this._headJ = j;     
+        this._headValue = x;   
       }
       this._gameArray[i][j] = x;
-      x--;
+      x++;
     }
 
   }
@@ -215,10 +209,14 @@ export class HomePage implements OnInit {
       this._gameArray[this._headI][this._headJ] = this._headValue;
       this._numberOfTreats--;
     } else if (this._gameArray[this._headI][this._headJ] > 0) { //snake bit self
-      // GAME OVER
+      this.gameOver()
     }
   }
 
+  gameOver(){
+    this._playing = false;
+    this._gameFinished = true;
+  }
   timers() {
 
     setInterval(() => {
@@ -233,7 +231,7 @@ export class HomePage implements OnInit {
         this.moveSnake();
         this._canTurn = true;
       }
-    }, 1000 / 15);
+    }, 1000 / 12);
   }
 
   generateTreats() {
@@ -253,10 +251,21 @@ export class HomePage implements OnInit {
   pause(){
     if(this._playing){
     this._playing = false;
-    } else {
+    } else {      
+      if(this._gameFinished) {
+        this.initArray();
+        this._gameFinished = false;
+      }
       this._playing = true;
     }
-    
+    console.log(this._gameFinished)
+    console.log(this._playing)
+  }
+
+  restart(){
+    this._playing = false;
+    this.initArray();
+    this.drawCanvas();
   }
 
   getIsPlaying():boolean {
